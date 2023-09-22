003689886c538b598e167869e5236e98e3532569
TeddyFer06 <72159786+TeddyFer06@users.noreply.github.com>
2023-09-21 23:37:51 -0500

    Completing a project with the elimination of a customer

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,4 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Form, redirect } from "react-router-dom";
+import { eliminarCliente } from '../data/clientes'
+
+export async function action({ params }) {
+    await eliminarCliente(params.clienteId)
+    return redirect('/')
+}
 
 const Cliente = ({ cliente }) => {
 
@@ -26,12 +32,23 @@ const Cliente = ({ cliente }) => {
                 >
                     Editar
                 </button>
-                <button
-                    type="button"
-                    className="text-red-600 hover:text-red-700 uppercase text-xs"
+
+                <Form
+                    method="post"
+                    action={`/clientes/${id}/eliminar`}
+                    onSubmit={(e) => {
+                        if (!confirm('Deseas eliminar este registro?')) {
+                            e.preventDefault()
+                        }
+                    }}
                 >
-                    Eliminar
-                </button>
+                    <button
+                        type="submit"
+                        className="text-red-600 hover:text-red-700 uppercase text-xs"
+                    >
+                        Eliminar
+                    </button>
+                </Form>
             </td>
         </tr>
     )
